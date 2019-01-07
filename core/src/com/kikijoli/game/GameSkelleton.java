d63b1f5b6a5ae28d2340bb92ae5cce405ad5f078
ajosseau <a-josseau@groupeo2i.com>
2019-01-07 11:28:33 +0100

    Remove some useless line of code

--- core/src/com/kikijoli/game/GameSkelleton.java
+++ core/src/com/kikijoli/game/GameSkelleton.java
@@ -3,8 +3,6 @@ package com.kikijoli.game;
 import com.badlogic.gdx.Game;
 import com.kikijoli.game.maps.Tmap;
 import com.kotcrab.vis.ui.VisUI;
-import de.tomgrill.gdxdialogs.core.GDXDialogs;
-import de.tomgrill.gdxdialogs.core.GDXDialogsSystem;
 
 public class GameSkelleton extends Game {
 
